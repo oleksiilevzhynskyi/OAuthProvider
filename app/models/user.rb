3890974affac2fa8f5959a1dc0dde797ebc2569d
Alexey Levzhinsky <alex_lev89@mail.ru>
2011-08-16 23:42:46 +0300

    Configured devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  devise :database_authenticatable, :registerable, :confirmable, :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable
 
   validates :login, :presence => true
   validates :name, :presence => true
