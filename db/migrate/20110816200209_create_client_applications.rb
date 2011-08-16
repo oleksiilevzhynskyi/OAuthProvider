b97d6eab8454d01ef4fef784317a831a3337d8d3
Alexey Levzhinsky <alex_lev89@mail.ru>
2011-08-16 23:42:24 +0300

    Generated modle client application

---
+++ db/migrate/20110816200209_create_client_applications.rb
@@ -0,0 +1,15 @@
+class CreateClientApplications < ActiveRecord::Migration
+  def change
+    create_table :client_applications do |t|
+      t.string :name
+      t.string :callback_url
+      t.string :access_level
+      t.string :consumer_key
+      t.string :consumer_secret
+      t.integer :access_token_id
+
+      t.timestamps
+    end
+  end
+end
+
