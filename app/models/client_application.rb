f5f56c7dba814f2e764d9036097a2b14d031b235
Alexey Levzhinsky <alex_lev89@mail.ru>
2011-08-17 23:07:46 +0300

    Added random_string function

--- app/models/client_application.rb
+++ app/models/client_application.rb
@@ -1,2 +1,8 @@
 class ClientApplication < ActiveRecord::Base
+
+  def random_sting(options = {})
+    alphabet = [('a'..'z'),('A'..'Z'),(0..9)].map{|i| i.to_a}.flatten.suffle
+    options[:size].map{ alphabet[rand(alphabet.length)]  }.join
+  end
 end
+
