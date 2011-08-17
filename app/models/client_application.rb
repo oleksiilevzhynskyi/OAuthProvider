1c2d12189aa7d7b7a913be94c460dc08c7bd5572
Alexey Levzhinsky <alex_lev89@mail.ru>
2011-08-18 00:23:57 +0300

    Added before save to model client_application

--- app/models/client_application.rb
+++ app/models/client_application.rb
@@ -2,9 +2,20 @@ class ClientApplication < ActiveRecord::Base
 
   belongs_to :users
 
+  before_save :create_keys
+
+  private
+
   def random_sting(options = {})
-    alphabet = [('a'..'z'),('A'..'Z'),(0..9)].map{|i| i.to_a}.flatten.suffle
-    options[:size].map{ alphabet[rand(alphabet.length)]  }.join
+    alphabet = [('a'..'z'),('A'..'Z'),(0..9)].map{|i| i.to_a}.flatten.shuffle
+    (1..options[:size]).map{ alphabet[rand(alphabet.length)]  }.join
+  end
+
+  def create_keys
+      self.callback_url = nil
+      self.access_level = nil
+      self.consumer_key = random_sting :size => 20
+      self.consumer_secret = random_sting :size => 40
   end
 end
 
